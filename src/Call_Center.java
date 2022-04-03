6691fce9465cfa9ae47bce5028a8690bb384e7ce
AASukhov <arkhonfills@list.ru>
2022-04-03 03:46:09 +0300

    Initial commit

---
+++ src/Call_Center.java
@@ -0,0 +1,33 @@
+import java.util.Queue;
+import java.util.concurrent.ArrayBlockingQueue;
+
+public class Call_Center {
+    private static final int CALLS = 60;
+    private static final Queue <String> phoneCalls = new ArrayBlockingQueue<>(CALLS);
+    private static final int PAUSE = 100;
+    private static final int ANSWER = 3000;
+
+    public void call() {
+        try{
+            for (int i = 0; i < CALLS; i++) {
+                phoneCalls.offer("Звонок №" + i);
+                System.out.println("Поступил звонок №" + i);
+                Thread.sleep(PAUSE);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void takingCall() {
+        while(!phoneCalls.isEmpty()) {
+            try {
+                Thread.sleep(ANSWER);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("Оператор " + Thread.currentThread().getName() + " ответил на звонок " + phoneCalls.poll());
+        }
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static final int EMPLOYEES = 10;
+
+    public static void main(String[] args) {
+        Call_Center center = new Call_Center();
+
+        new Thread(null, center::call, "Звонок №").start();
+
+        for (int i = 0; i <= EMPLOYEES; i++) {
+            new Thread(null, center::takingCall, "Оператор №" + i).start();
+        }
+    }
+}
