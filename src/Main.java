70f3c552428eea557bde7279894e8b0b28cbdd75
AASukhov <arkhonfills@list.ru>
2022-04-03 03:49:55 +0300

    The second commit

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         Call_Center center = new Call_Center();
 
-        new Thread(null, center::call, "Звонок №").start();
+        new Thread(null, center::call, "Поток АТС").start();
 
         for (int i = 0; i <= EMPLOYEES; i++) {
             new Thread(null, center::takingCall, "Оператор №" + i).start();
